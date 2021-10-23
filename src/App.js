4337f450d6a158632829a2209fe80a9ed5f9e32b
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-10-23 17:59:13 -0400

    datos desde un archivo Json

--- src/App.js
+++ src/App.js
@@ -2,33 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json';
 
 function App() {
 
-  let peliculas = [
-    {
-      titulo:"oblivion", 
-      año:"(2012)", 
-      calificacion:"8.1", 
-      director:"Joss whedon", 
-      actores:"Robert Downey Jr., Chris Evans, Chris Hemsworth", 
-      fecha:"1 May 2015", 
-      duracion:"2h 21min", 
-      img:"images/uploads/mv1.jpg",
-      descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-      titulo:"into the wild",
-      año:"(2014)",
-      calificacion:"7.8",
-      director:"Anthony Russo, Joe Russo",
-      actores:"Chris Evans, Samuel L. Jackson, Scarlett Johansson",
-      fecha:"1 May 2015",
-      duracion:"2h 21min",
-      img:"images/uploads/mv2.jpg",
-      descripcion:"As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat..."
-    }
-  ];
+  let peliculas = peliculasJson;
   return (
         <PageWrapper>
             <Pelicula titulo="oblivion" año="(2012)" calificacion="8.1" director="Joss whedon" actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" fecha="1 May 2015" duracion="2h 21min" img="images/uploads/mv1.jpg">
